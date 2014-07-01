773cc515b0384988f586a0296b0dafbed332fcd7
Michael Novotny <manovotny@manovotny-rmbp.local>
2014-07-01 05:24:17 -0500

    Fixes typo in config `repository` property name.

--- config/default.js
+++ config/default.js
@@ -19,7 +19,7 @@ module.exports = (function () {
             from: 'NAME',
             to: 'NAME'
         },
-        repositroy: {
+        repository: {
             from: 'REPOSITORY',
             to: 'REPOSITORY'
         },
