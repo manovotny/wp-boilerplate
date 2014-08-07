0f8a530cc33bdc5f9705e6fef2a843fb5034a247
Michael Novotny <manovotny@gmail.com>
2014-08-07 00:08:39 -0500

    Moves previous text replace values to their own file and fixes description replacement.

---
+++ config/previous.js
@@ -0,0 +1,24 @@
+module.exports = (function () {
+
+    'use strict';
+
+    return {
+        author: {
+            email: 'AUTHOR_EMAIL',
+            name: 'AUTHOR_NAME',
+            url: 'AUTHOR_URL'
+        },
+        project: {
+            composer: 'PROJECT_COMPOSER',
+            copyright: 'PROJECT_COPYRIGHT',
+            description: 'PROJECT_DESCRIPTION',
+            git: 'PROJECT_GIT',
+            name: 'PROJECT_NAME',
+            package: 'PROJECT_PACKAGE',
+            slug: 'PROJECT_SLUG',
+            url: 'PROJECT_URL',
+            version: '0.0.0'
+        }
+    };
+
+}());
