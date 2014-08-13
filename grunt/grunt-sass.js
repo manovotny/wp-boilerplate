0a33c9aff52bdbe4a7144f24cc49c37df6fa798c
Michael Novotny <manovotny@gmail.com>
2014-08-12 22:51:10 -0500

    Fixes grunt-sass not generating CSS.

--- grunt/grunt-sass.js
+++ grunt/grunt-sass.js
@@ -6,38 +6,36 @@ module.exports = function (grunt) {
         expand = true,
         extension = '.css',
         options = {
-            outputStyle: 'compressed'
+            outputStyle: 'nested'
         },
         source = [
             '**/*.scss'
         ];
 
     grunt.config('sass', {
-        sass: {
-            admin: {
-                options: options,
-                files: [
-                    {
-                        expand: expand,
-                        cwd: config.paths.admin + '/' + config.paths.sass,
-                        src: source,
-                        dest: config.paths.admin + '/' + config.paths.css,
-                        ext: extension
-                    }
-                ]
-            },
-            site: {
-                options: options,
-                files: [
-                    {
-                        expand: expand,
-                        cwd: config.paths.sass,
-                        src: source,
-                        dest: config.paths.css,
-                        ext: extension
-                    }
-                ]
-            }
+        admin: {
+            options: options,
+            files: [
+                {
+                    expand: expand,
+                    cwd: config.paths.admin + '/' + config.paths.sass,
+                    src: source,
+                    dest: config.paths.admin + '/' + config.paths.css,
+                    ext: extension
+                }
+            ]
+        },
+        site: {
+            options: options,
+            files: [
+                {
+                    expand: expand,
+                    cwd: config.paths.sass,
+                    src: source,
+                    dest: config.paths.css,
+                    ext: extension
+                }
+            ]
         }
     });
 
