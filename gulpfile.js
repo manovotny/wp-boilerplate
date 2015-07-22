47eb607d617cbb151996ea028499289b8bc01f37
Michael Novotny <manovotny@gmail.com>
2015-07-21 23:28:15 -0500

    Adds Browserify.

--- gulpfile.js
+++ gulpfile.js
@@ -16,3 +16,41 @@ gulp.task('eslint', function () {
         .pipe(eslint.format())
         .pipe(eslint.failOnError());
 });
+
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var globby = require('globby');
+var through = require('through2');
+var gutil = require('gulp-util');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('browserify', function () {
+    var bundleStream = through();
+
+    bundleStream
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .on('error', gutil.log)
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./src/js/'));
+
+    globby(['./entries/**/*.js'], function(error, entries) {
+        if (error) {
+            bundleStream.emit('error', error);
+            return;
+        }
+
+        var b = browserify({
+            entries: entries,
+            debug: true
+        });
+
+        b.bundle().pipe(bundleStream);
+    });
+
+    return bundleStream;
+});
